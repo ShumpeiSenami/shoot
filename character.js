8b8192546ac6e52b1fd0dd0dc6cefd313cb3a956
shun <kaifenan@icloud.com>
2019-01-15 09:18:57 +0900

    first

---
+++ character.js
@@ -0,0 +1,47 @@
+
+
+//-自機のcharacterクラス作成-----------------------------------
+function Character(){
+    this.position = new Point();
+    this.size = 0;
+};
+
+//自機のサイズ指定を init メソッドで行う
+Character.prototype.init = function(size){
+    this.size = size;
+};
+
+//-自機の弾を扱う CharacterShotoクラスの作成-------------------
+function CharacterShot(){
+    this.position = new Point();
+    this.size = 0;
+    this.speed = 0;
+    this.alive = false;
+}
+ //set メソッドは引数を３つほど受け取りそれと共に自機ショットを初期化
+ //引数にはショットの初期位置とサイズ、スピード与えます
+ //このメソッドが呼び出されることで this.alive=true も自動で呼び出され生存フラグが立つ
+ CharacterShot.prototype.set = function(p, size, speed){
+    //座標をセット
+    this.position.x = p.x;
+    this.position.y = p.y;
+
+    //サイズ、スピードをセット
+    this.size = size;
+    this.speed = speed;
+
+    //生存フラグを立てる
+    this.alive = true;
+ };
+ // moveメソッドはショットの動きに関する処理を持つ
+ //自機は画面下から画面上に向かってショットを打つので座標では　y軸は数字が小さくなる（左上が座標０、０のため)
+
+ CharacterShot.prototype.move = function(){
+    // 座標を真上にspeed分だけ移動させる
+    this.position.y -= this.speed;
+
+    //一定以上の座標に到達していたら生存フラグを降ろす
+    if(this.position.y < -this.size){
+        this.alive = false;
+    }
+ };
---
+++ commo.js
@@ -0,0 +1,6 @@
+
+//xとyの二つの座標情報を格納するためのクラス
+function Point(){
+    this.x = 0;
+    this.y =0;
+}
